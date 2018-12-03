653e497fa884d9ac0806b8b9c14f9cef19a8f530
ghaniturismo <ghaniturismo@gmail.com>
2018-12-03 17:49:53 +0100

    [ADD] formateurs

--- src/app/formateurs/formateurs.component.ts
+++ src/app/formateurs/formateurs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Formateur} from '../shared/interfaces/formateur';
 
 @Component({
   selector: 'app-formateurs',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormateursComponent implements OnInit {
 
-  constructor() { }
+  private _formateur: Formateur[];
+
+  constructor() {
+    this._formateur = [];
+  }
 
   ngOnInit() {
+    // TODO : fetch with associated service formateur
   }
 
 }
