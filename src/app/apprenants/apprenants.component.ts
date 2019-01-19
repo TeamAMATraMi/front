0bf51b11c97cfc4f6b237786098c0eaeed2bd162
Tarek <dalati.m.t@hotmail.fr>
2019-01-19 17:35:34 +0100

    Filtre groupe en fonction du site

--- src/app/apprenants/apprenants.component.ts
+++ src/app/apprenants/apprenants.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Apprenant} from '../shared/interfaces/apprenant';
 import {ApprenantsService} from '../shared/services/apprenants.service';
 import {Router} from '@angular/router';
@@ -17,12 +17,15 @@ export class ApprenantsComponent implements OnInit {
   private _apprenants: Apprenant[];
   private _sites: Site[];
   private  _groupes: Groupe[];
+  private _site: Site;
+  private _groupesSite: Groupe[];
 
   constructor(private _router: Router, private _apprenantsService: ApprenantsService, private _sitesService: SitesService,
               private _groupesService: GroupesService) {
     this._apprenants = [];
     this._sites = [];
     this._groupes = [];
+    this._groupesSite = [];
   }
 
 
@@ -34,8 +37,23 @@ export class ApprenantsComponent implements OnInit {
     return this._sites;
   }
 
+  setSite(site: Site) {
+    this._site = site;
+    this._groupesSite = [];
+    this._groupes.forEach(e => {
+          if (e.idSite === this._site.id) {
+            this._groupesSite.push(e);
+          }
+        }
+    );
+  }
+
   get groupes(): Groupe[] {
     return this._groupes;
+  }
+
+  get groupesSites(): Groupe[] {
+    return this._groupesSite;
   }
 
 
@@ -49,7 +67,7 @@ export class ApprenantsComponent implements OnInit {
       // TODO : fetch with associated service
     this._apprenantsService.fetch().subscribe((apprenants: Apprenant[]) => this._apprenants = apprenants);
     this._sitesService.fetch().subscribe((sites: Site[]) => this._sites = sites);
-    this._groupesService.fetch().subscribe((groupes: Groupe[]) => this._groupes = groupes);
+    this._groupesService.fetch().subscribe((groupes: Groupe[]) => { this._groupes = groupes; this._groupesSite = this._groupes; });
   }
 
 
