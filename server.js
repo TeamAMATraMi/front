3ea303f39e1f7f4080f40238a529fab0f78a863e
Frodonche <guiguix3@hotmail.fr>
2019-01-25 17:30:47 +0100

    Je passe le relai

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/deploy/applitrami-front/src'));
+app.use(express.static(__dirname + '/deploy/applitrami-front/src/'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'index.html'));
