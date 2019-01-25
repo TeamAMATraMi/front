dce2c48028aeadb18062285d215dbce0825daade
Frodonche <guiguix3@hotmail.fr>
2019-01-25 17:06:58 +0100

    peut etre reussi

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/appliTrami'));
+app.use(express.static(__dirname + '/deploy/applitrami-front/src'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/appliTrami/index.html'));
+  res.sendFile(path.join(__dirname+'/deploy/applitrami-front/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
