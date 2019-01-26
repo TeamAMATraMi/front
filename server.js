fbe977f71e42ae17e2210a7e76e0abd20975c3f1
Laura-Weber <laura.weber57@laposte.net>
2019-01-26 11:04:11 +0100

    test8

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/deploy/applitrami-front/src/'));
+app.use(express.static(__dirname + '/dist/applitrami-front'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'index.html'));
+  res.sendFile(path.join(__dirname+'/dist/applitrami-front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
