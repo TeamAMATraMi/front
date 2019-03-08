c70208c934ef3d27c541086bd3662717f25861d9
Unknown <e.hicham95@gmail.com>
2019-03-08 12:03:07 +0100

    Fix cours display

--- src/app/groupe/groupe.component.ts
+++ src/app/groupe/groupe.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Groupe} from '../shared/interfaces/groupe';
 import {filter} from 'rxjs/operators';
@@ -7,6 +7,7 @@ import {Apprenant} from '../shared/interfaces/apprenant';
 import {ApprenantsService} from '../shared/services/apprenants.service';
 import {Site} from '../shared/interfaces/site';
 import {SitesService} from '../shared/services/sites.service';
+import {MatPaginator, MatSort, MatTableDataSource, Sort} from '@angular/material';
 
 @Component({
   selector: 'app-groupe',
@@ -19,7 +20,11 @@ export class GroupeComponent implements OnInit {
   private _apprenants: Apprenant[];
   private _sites: Site[];
   private readonly _delete$: EventEmitter<Groupe>;
+  private _dataSource: MatTableDataSource<Apprenant>;
+  private _displayedColumns = ['NomPrenom', 'DateNaissance', 'PaysOrigine'];
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private _apprenantsService: ApprenantsService, private _route: ActivatedRoute, private _sitesService: SitesService) {
     this._groupe = {} as Groupe;
@@ -53,6 +58,16 @@ export class GroupeComponent implements OnInit {
     )
         .subscribe((apprenants: Apprenant[]) => {
           this._apprenants = apprenants;
+          this._dataSource = new MatTableDataSource<Apprenant>(apprenants);
+          this._dataSource.paginator = this.paginator;
+          this._dataSource.sort = this.sort;
+          this._dataSource.filterPredicate = (data: {nom: string, prenom: string}, filterValue: string) => {
+            if ((data.nom.trim().toLowerCase().indexOf(filterValue) !== -1) || (data.prenom.trim().toLowerCase().indexOf(filterValue) !== -1)) {
+              return true;
+            } else {
+              return false;
+            }
+          };
         });
   }
 
@@ -64,4 +79,35 @@ export class GroupeComponent implements OnInit {
   delete(groupe: Groupe) {
     this._delete$.emit(groupe);
   }
+
+  get displayedColumns(): any {
+    return this._displayedColumns;
+  }
+
+  get dataSource(): MatTableDataSource<Apprenant> {
+    return this._dataSource;
+  }
+
+  sortData(sort: Sort) {
+    const data = this._dataSource.data.slice();
+    if (!sort.active || sort.direction === '') {
+      this._dataSource.data = data;
+      return;
+    }
+    this._dataSource.data = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'NomPrenom':
+          return compare(a.nom, b.nom, isAsc);
+        case 'PaysOrigine':
+          return compare(a.paysOrigine, b.paysOrigine, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
