849dfc73a10e7cb4b48e7988d8aa46efc98d9c2e
ghaniturismo <ghaniturismo@gmail.com>
2019-03-11 19:43:00 +0100

    [FIX] conflict with travis

--- src/app/formateurs/formateurs.component.ts
+++ src/app/formateurs/formateurs.component.ts
@@ -36,7 +36,7 @@ export class FormateursComponent implements OnInit {
   private _formateursDialog: MatDialogRef<FormateurDialogComponent>;
   private _dataSource: MatTableDataSource<Formateur>;
 
-  private _value = '';
+  value = '';
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -160,10 +160,6 @@ export class FormateursComponent implements OnInit {
         .pipe(
             flatMap(_ => this._formateursService.fetch())
         );
-  }
-
-  get value(): string {
-    return this._value;
   }
 
   @Input()
