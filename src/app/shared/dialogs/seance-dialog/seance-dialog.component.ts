324800883c880e0a13209b1960846ee1c81646bd
Yougo <wodhugo@gmail.com>
2020-02-13 16:56:27 +0100

    dialog pour l'ajout des senaces

---
+++ src/app/shared/dialogs/seance-dialog/seance-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {Cours} from '../../interfaces/cours';
+import {Seance} from '../../interfaces/seance';
+
+@Component({
+  selector: 'app-seance-dialog',
+  templateUrl: './seance-dialog.component.html',
+  styleUrls: ['./seance-dialog.component.css']
+})
+export class SeanceDialogComponent implements OnInit {
+
+  constructor(private _dialogRef: MatDialogRef<SeanceDialogComponent>, @Inject(MAT_DIALOG_DATA) private _seance: Seance) { }
+
+  ngOnInit() {
+  }
+
+  get seance(): Seance {
+    return this._seance;
+  }
+
+  onCancel() {
+    this._dialogRef.close();
+  }
+
+  onSave(cours: Cours) {
+    this._dialogRef.close(cours);
+  }
+}
