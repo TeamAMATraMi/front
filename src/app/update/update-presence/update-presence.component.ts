6df6eb9daba997c944ba2916142fafef29690fcf
AlexGinestra <alex.ginestra4@etu.univ-lorraine.fr>
2020-03-07 17:48:35 +0100

    liste etudiant a une seance

--- src/app/update/update-presence/update-presence.component.ts
+++ src/app/update/update-presence/update-presence.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material';
 import {filter, flatMap, map} from 'rxjs/operators';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {PresenceDialogComponent} from '../../shared/dialogs/presence-dialog/presence-dialog.component';
 import {PresencesService} from '../../shared/services/presences.service';
 import {Presence} from '../../shared/interfaces/presence';
+import {Seance} from '../../shared/interfaces/seance';
+import {Apprenant} from '../../shared/interfaces/apprenant';
+import {Cours} from '../../shared/interfaces/cours';
+import {CoursService} from '../../shared/services/cours.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {ApprenantsService} from '../../shared/services/apprenants.service';
+import {SelectionModel} from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-update-presence',
@@ -13,37 +20,90 @@ import {Presence} from '../../shared/interfaces/presence';
 })
 export class UpdatePresenceComponent implements OnInit {
 
-  private _presenceDialog: MatDialogRef<PresenceDialogComponent>;
+  private _seance: Seance;
+  private _dataSource: MatTableDataSource<Apprenant>;
+  private _cours: Cours;
+  private _presences: Presence[];
+  private _selection: SelectionModel<Apprenant>;
+  displayedColumns: String[] = ['nom', 'prenom', 'id'];
+
 
   constructor(private _route: ActivatedRoute, private _router: Router, private _presencesService: PresencesService,
-              private _dialog: MatDialog) { }
+              private _apprenantsService: ApprenantsService, private _coursService: CoursService, private _dialog: MatDialog) {
+      this._seance = {} as Seance;
+      this._cours = {} as Cours;
+      this._presences = [];
+      const initialSelection = [];
+      const allowMultiSelect = true;
+      this._selection = new SelectionModel<Apprenant>(allowMultiSelect, initialSelection);
+  }
 
   ngOnInit() {
-      console.log("UpdatePresenceComponent", "ngOnInit");
-    this._route.params
-        .pipe(
-            filter(params => {
-                console.log("params", params);
-                return !!params['id'];
-            }),
-            flatMap(params => {
-              return this._presencesService.fetchByIdSeance(params['id'])})
-        )
-        .subscribe((presences: Presence[]) => {
-          this._presenceDialog = this._dialog.open(PresenceDialogComponent, {
-            width: '500px',
-            disableClose: true,
-            data: presences
-          });
-
-          // subscribe to afterClosed observable to set dialogs status and do process
-          this._presenceDialog.afterClosed()
-              .pipe(
-                  filter(_ => !!_),
-                  flatMap(_ => this._presencesService.update(_))
-              )
-              .subscribe(null, null, () => this._router.navigate([ '/cours' ]));
+      console.log('UpdatePresenceComponent', 'ngOnInit');
+      this._route.params
+        .subscribe((params: Params) => {
+            const idCours = +params['idCours'];
+            this._coursService.fetchOne(idCours).subscribe((cours: Cours) => {
+                this._cours = cours;
+                this._apprenantsService.fetchByGroup(this._cours.idGroupe).subscribe((apprenants: Apprenant[]) => {
+                    this._dataSource = new MatTableDataSource<Apprenant>(apprenants);
+                });
+            });
+            this._presencesService.fetchByIdSeance(params['id']).subscribe((presences: Presence[]) => {
+                this._presences = presences;
+            });
         });
   }
+
+  get dataSource(): MatTableDataSource<Apprenant> {
+    return this._dataSource;
+  }
+
+  set dataSource(value: MatTableDataSource<Apprenant>) {
+      this._dataSource = value;
+  }
+
+  get seance(): Seance {
+      return this._seance;
+  }
+
+  set seance(value: Seance) {
+      this._seance = value;
+  }
+
+  get cours(): Cours {
+      return this._cours;
+  }
+
+  get presences(): Presence[] {
+      return this._presences;
+  }
+
+  get selection(): SelectionModel<Apprenant> {
+      return this._selection;
+  }
+
+
+    /** Whether the number of selected elements matches the total number of rows. */
+    isAllSelected() {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.dataSource.data.length;
+        return numSelected == numRows;
+    }
+
+    /** Selects all rows if they are not all selected; otherwise clear selection. */
+    masterToggle() {
+        this.isAllSelected() ?
+            this.selection.clear() :
+            this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+
+
+    /** Validation des presences **/
+    validatePresences() {
+
+        alert('Présences enregistrées');
+    }
+
 
 }
