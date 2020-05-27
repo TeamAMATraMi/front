b6ccd40fd6d095707a12152fb1dc8fd2c9d7f567
Nada <madadnada@gmail.com>
2020-05-27 18:59:16 +0200

    Update pour deployer

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -30,6 +30,10 @@ constructor(private _coursService : CoursService){
 this._cours=[];
 }
 
+get calendarVisible(): boolean {
+    return this.calendarVisible;
+  }
+
 ngOnInit(){
 
 this.calendarVisible=true;
